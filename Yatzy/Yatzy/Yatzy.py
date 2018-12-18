151972fbd7cdcd5c6b0377bbfe18268b057a7465
Viktor <viktor.johansson96@gmail.com>
2018-12-18 11:51:58 +0100

    Base Structure

--- Yatzy/Yatzy/Yatzy.py
+++ Yatzy/Yatzy/Yatzy.py
@@ -1,5 +1,28 @@
+
+dices = [0,0,0,0,0]
+
+scoreOne = 0
+scoretwo = 0
+
 def main ():
-    print("Hello World")
+    while (True):
+        input("Please enter your name: ")
+        lista = [0,1,2,3]
+        for x in lista:
+            print (x)
+        lista = "Hej"
+        print (lista)
+
+
+def roll ():
+    print ("Not Defined")
+
+def winner ():
+    print ("Not Defined")
+
+def sum ():
+    print ("Not Defined")
+
 
 if __name__ == "__main__": main()
 
