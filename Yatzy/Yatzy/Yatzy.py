cc6a65064c4961e8436501170fa3821b11c2bac1
Izhya <timcarlsson.tc@gmail.com>
2018-12-18 14:05:32 +0100

    Score?

--- Yatzy/Yatzy/Yatzy.py
+++ Yatzy/Yatzy/Yatzy.py
@@ -5,14 +5,14 @@ scoreOne = 0
 scoretwo = 0
 
 def main ():
-    while (True):
-        input("Please enter your name: ")
-        lista = [0,1,2,3]
-        for x in lista:
-            print (x)
-        lista = "Hej"
-        print (lista)
-
+   ## while (true):
+   #     input("please enter your name: ")
+   #     lista = [0,1,2,3]
+   #     for x in lista:
+   #         print (x)
+   #     lista = "hej"
+   #     print (lista)
+    print("Hejsan")
 
 def roll ():
     print ("Not Defined")
@@ -20,10 +20,22 @@ def roll ():
 
 
 def winner ():
-    print ("Not Defined")
+    if (scoreOne > scoretwo):
+        print("Spelare 1 vann med : " + p1sum())
+    if (scoretwo > scoreOne):
+        print("Spelare 2 vann med : " + p2Sum()) 
+    else:
+        print("Oavgjort")
 
 def sum ():
-    print ("Not Defined")
+    if (turn == playerOneT):
+        for x in savedDice:
+            score += savedDice
+    if (turn == playerTwoT):
+        for x in savedDice:
+            score += savedDice
+
+    
 
 
 if __name__ == "__main__": main()
