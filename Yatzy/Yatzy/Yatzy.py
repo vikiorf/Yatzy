c5670ef41eb0d1b9b386e4703ab96adf504d37f0
Izhya <timcarlsson.tc@gmail.com>
2018-12-18 15:35:01 +0100

    Tärningsräkning

--- Yatzy/Yatzy/Yatzy.py
+++ Yatzy/Yatzy/Yatzy.py
@@ -1,49 +1,45 @@
 
 dices = [0,0,0,0,0]
-
 scoreOne = 0
-scoretwo = 0
+scoreTwo = 0
+turn = ""
 
 import random
 
 def main ():
-    while (True):
         input("Please enter your name: ")
         lista = [0,1,2,3]
-        #for x in lista:
-            #print (x)
-        lista = "Hej"
-        #print (lista)  
-
-        print(roll())
-
+       
+        dices = roll()
+        print(dices)
+        turn = "playerOneTurn"
+        sumTot()
+        print(scoreOne)
 
 def roll ():    
     
     dices=[random.randint(1, 6) for _ in range(6)]
     return dices
 
-
-
 def winner ():
-    if (scoreOne > scoretwo):
+    if (scoreOne > scoreTwo):
         print("Spelare 1 vann med : " + p1sum())
-    if (scoretwo > scoreOne):
+    if (scoreTwo > scoreOne):
         print("Spelare 2 vann med : " + p2Sum()) 
     else:
         print("Oavgjort")
 
-def sum ():
-    if (turn == playerOneT):
-        for x in savedDice:
-            score += savedDice
-    if (turn == playerTwoT):
-        for x in savedDice:
-            score += savedDice
+def sumTot ():
+    if (turn == "playerOneTurn"):
+       scoreOne = 0
+       for x in dices:
+            scoreOneTotal += x
+
+    if (turn == "playerTwo"):
+        scoreTwo = 0
+        for x in dices:
+            scoreTwo += x
 
     
-
-
-
 if __name__ == "__main__": main()
 
