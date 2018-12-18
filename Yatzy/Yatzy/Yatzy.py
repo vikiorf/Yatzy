6dbfba363708c7a5d71180b47ae569df04e5a129
Viktor <viktor.johansson96@gmail.com>
2018-12-18 14:05:39 +0100

    Add roll function

--- Yatzy/Yatzy/Yatzy.py
+++ Yatzy/Yatzy/Yatzy.py
@@ -4,24 +4,30 @@ dices = [0,0,0,0,0]
 scoreOne = 0
 scoretwo = 0
 
+import random
+
 def main ():
     while (True):
         input("Please enter your name: ")
         lista = [0,1,2,3]
-        for x in lista:
-            print (x)
+        #for x in lista:
+            #print (x)
         lista = "Hej"
-        print (lista)
+        #print (lista)  
 
+        print(roll())
 
-def roll ():
-    print ("Not Defined")
+def roll ():    
+    
+    dices=[random.randint(1, 6) for _ in range(6)]
+    return dices
 
 def winner ():
     print ("Not Defined")
 
 def sum ():
     print ("Not Defined")
+
 
 
 if __name__ == "__main__": main()
